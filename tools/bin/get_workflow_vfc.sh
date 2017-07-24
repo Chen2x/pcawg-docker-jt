48437c4efbf513bedce791a12a6d12c5a1596a71
Chen2x <chenchen199727@gmail.com>
2017-07-24 15:54:13 -0400

    delete useless scripts

--- tools/bin/get_workflow_vfc.sh
+++
@@ -1,6 +0,0 @@
-local donor=$1
-
-if [ ! -f $base_dir/data/$donor/broad.oxoG.vcf.gz ]; then
-	local donor_id_dir="$icgc_dir/$donor/ID"
-         	$base_dir/bin/get_gnos_workflow_results.sh $donor
-fi
