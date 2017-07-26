e39174a9f3f20a2179cf816c4569b46a4665d625
Chen2x <chenchen199727@gmail.com>
2017-07-26 15:47:55 -0400

    clean up yaml

--- tools/BWA-MEM.py
+++ tools/BWA-MEM.py
@@ -1,7 +1,11 @@
 import subprocess
 
+
+donor = "DO46792"
+source = "ICGC"
+
 try:
-    print (subprocess.checkoutput(["run_workflow.sh", "BWA-MEM", donor, gnos_or_igcg]))
+    print (subprocess.checkoutput(["run_workflow.sh", "BWA-MEM", donor, source]))
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
         sys.exit(1)
